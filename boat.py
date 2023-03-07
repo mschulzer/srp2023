6620738cdf19ad66a9439614d4296896fa93f77f
Morten Schultz <45948423+mschulzer@users.noreply.github.com>
2023-03-07 15:38:58 +0100

    Update boat.py

--- boat.py
+++ boat.py
@@ -4,6 +4,10 @@ import numpy as np
 import math
 
 
+###################################################################
+# Definér nogle funktioner
+###################################################################
+
 def erasableWrite(tortoise, name, font, align, reuse=None):
     eraser = Turtle() if reuse is None else reuse
     eraser.hideturtle()
@@ -11,8 +15,6 @@ def erasableWrite(tortoise, name, font, align, reuse=None):
     eraser.setposition(tortoise.position())
     eraser.write(name, font=font, align=align)
     return eraser
-    
-
 
 def vind(x):
     Fs = 8000
@@ -22,18 +24,23 @@ def vind(x):
     y = np.sin(2 * np.pi * f * x/8000)
     return y
 
-# rV((2*pi*r)^2)*w*L
+# Magnus-effekten: rV((2*pi*r)^2)*w*L
 def magnus(v):
     return ((math.pow((2*math.pi*2.5), 2))*1.225*30*1*v)
 
 def newton_kg(x):
     return (x*0.1019716)
 
+###################################################################
+# Sæt nogle variable - og initier Turtle()
+###################################################################
+
 t = Turtle()
 setup(800, 500)
 bgcolor("lightskyblue")
 
 
+""" Main """
 for i in range(100):
     t.forward(8)
     if (i % 4 == 0):
